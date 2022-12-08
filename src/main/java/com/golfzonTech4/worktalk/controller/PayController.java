6fa6721f38057d1ed4267ad829cec4b71fda99b6
Fred Lee <juhee.fred.lee@gmail.com>
2022-12-08 18:09:58 +0900

    Updating Webhook logics

--- src/main/java/com/golfzonTech4/worktalk/controller/PayController.java
+++ src/main/java/com/golfzonTech4/worktalk/controller/PayController.java
@@ -1,11 +1,13 @@
 package com.golfzonTech4.worktalk.controller;
 
+import com.golfzonTech4.worktalk.domain.MyIamport;
 import com.golfzonTech4.worktalk.dto.pay.PayInsertDto;
 import com.golfzonTech4.worktalk.dto.pay.PayOrderSearch;
 import com.golfzonTech4.worktalk.dto.pay.PayWebhookDto;
 import com.golfzonTech4.worktalk.repository.ListResult;
 import com.golfzonTech4.worktalk.service.PayService;
 import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.response.Payment;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
@@ -19,6 +21,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class PayController {
     private final PayService payService;
+    private final MyIamport myIamport;
 
     /**
      * 선결제 데이터 검증 및 저장 요청
@@ -37,7 +40,10 @@ public class PayController {
     public void postpaid(
             @RequestBody PayWebhookDto dto) throws IamportResponseException, IOException {
         log.info("postpaid: dto");
-        payService.postpaid(dto);
+        if(dto.getMerchant_uid().contains("예약결제")){
+            log.info("heading to postpaid....");
+            payService.postpaid(dto);}
+
     }
 
     /**
