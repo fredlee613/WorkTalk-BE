2855641a4d613668b879e391d697136925036c29
Fred Lee <juhee.fred.lee@gmail.com>
2022-12-12 17:54:30 +0900

    Updating deploy.sh V2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl WorkTalk-BE | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -f worktalk-0.0.1.*.jar)
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
