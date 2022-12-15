ca04ace3e954d131ce69bcafea50b7f96a858e87
suyeon222 <qzwx604@naver.com>
2022-12-15 16:59:44 +0900

    카카오 소셜로그인 추가

---
+++ src/main/java/com/golfzonTech4/worktalk/service/KakaoJoinService.java
@@ -0,0 +1,45 @@
+package com.golfzonTech4.worktalk.service;
+
+import com.golfzonTech4.worktalk.domain.Member;
+import com.golfzonTech4.worktalk.dto.member.KakaoProfileDto;
+import com.golfzonTech4.worktalk.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.golfzonTech4.worktalk.domain.MemberType.ROLE_USER;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class KakaoJoinService {
+
+    private final MemberRepository memberRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public Long join(KakaoProfileDto dto) {
+        log.info("signup : {}", dto);
+
+        Member member = new Member();
+        member.setEmail(dto.getEmail());
+        member.setPw(dto.getPw());
+        member.setName(dto.getName());
+        member.setMemberType(ROLE_USER);
+        member.setImgName("profill.png");
+//        member.setKakaoYn("Y");
+        member.setPw(passwordEncoder.encode(member.getPw()));
+
+        return memberRepository.save(member).getId();
+    }
+
+//    @Transactional
+//    public String kakaoLogin(OAuth2KakaoDto dto) {
+//
+//    }
+
+}
