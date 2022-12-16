a4e30c65df4f3fd3d8feaf656341827fa9cad504
Fred Lee <juhee.fred.lee@gmail.com>
2022-12-16 18:32:56 +0900

    Updating RedissonFacade.java semaphore -> lock

--- src/main/java/com/golfzonTech4/worktalk/facade/RedissonFacade.java
+++ src/main/java/com/golfzonTech4/worktalk/facade/RedissonFacade.java
@@ -23,49 +23,57 @@ public class RedissonFacade {
     private final TempRedisReservationService redisReservationService;
     public final String LOCK_KEY = "LOCK_KEY";
 
-//    @Transactional
-//    public String chooseRoom(ReserveTempDto dto) throws InterruptedException {
-//        RLock lock = redissonClient.getLock(LOCK_KEY);
-//        List<String> result = new ArrayList<>();
-//        try {
-//            boolean available = lock.tryLock(5, 1, TimeUnit.SECONDS);
-//
-//            if (!available) {
-//                System.out.println("lock 획득 실패");
-//                return null;
-//            }
-//            String findKey = redisReservationService.reserveTemp(dto);
-//            result.add(findKey);
-//            log.info("findKey: {}", findKey);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        } finally {
-//            if (lock.isLocked() && lock.isHeldByCurrentThread()) {
-//                lock.unlock();
-//            }
-//        }
-//        return result.get(0);
-//    }
     @Transactional
     public String chooseRoom(ReserveTempDto dto) throws InterruptedException {
-        RSemaphore semaphore = redissonClient.getSemaphore("mySemaphore");
-        // 싱글 스레드 permit
-        semaphore.acquire();
-        // 싱글 스레드 permit & 15초 대기
-        boolean res = semaphore.tryAcquire(15, TimeUnit.SECONDS);
-
+        log.info("chooseRoom : {}", dto);
+        RLock lock = redissonClient.getLock(LOCK_KEY);
+        log.info("getLock....");
         List<String> result = new ArrayList<>();
+        try {
+            log.info("Try Lock ....");
+            boolean available = lock.tryLock(10, 5, TimeUnit.SECONDS);
+            log.info("Try Lock Done : available : {}", available);
 
-        if (res) {
-            try {
-                String findKey = redisReservationService.reserveTemp(dto);
-                result.add(findKey);
-                log.info("findKey: {}", findKey);
-            } finally {
-                semaphore.release();
+            if (!available) {
+                System.out.println("lock 획득 실패");
+                return null;
+            }
+            String findKey = redisReservationService.reserveTemp(dto);
+            result.add(findKey);
+            log.info("findKey: {}", findKey);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (lock.isLocked() && lock.isHeldByCurrentThread()) {
+                log.info("try unlock....");
+                lock.unlock();
+                log.info("unlock success....");
             }
         }
         return result.get(0);
     }
+//    @Transactional
+//    public String chooseRoom(ReserveTempDto dto) throws InterruptedException {
+//        log.info("chooseRoom: {}", dto);
+//        RSemaphore semaphore = redissonClient.getSemaphore("mySemaphore");
+//        log.info("get Semaphore...");
+//        // 싱글 스레드 permit & 5초 대기
+//        boolean res = semaphore.tryAcquire(5, 5, TimeUnit.SECONDS);
+//        log.info("try acquire Semaphore...");
+//        log.info("res: {}", res);
+//        List<String> result = new ArrayList<>();
+//
+//        if (res) {
+//            try {
+//                log.info("try....");
+//                String findKey = redisReservationService.reserveTemp(dto);
+//                result.add(findKey);
+//                log.info("findKey: {}", findKey);
+//            } finally {
+//                semaphore.release();
+//            }
+//        }
+//        return result.get(0);
+//    }
 
 }
